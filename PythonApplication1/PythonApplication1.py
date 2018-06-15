040959cde2a0d636ba7f2be256f49da12490fcd2
softwaretm <beisenov_b_b@mail.ru>
2018-06-15 23:02:06 +0300

    Syntax: classes

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -305,6 +305,92 @@ import math
 dir(math)
 
 
+####################################################
+## 6. Classes
+
+class Human:
+
+    # A class attribute. It is shared by all instances of this class
+    species = "H. sapiens"
+
+    # Note that the double leading and trailing underscores denote objects
+    # or attributes that are used by Python but that live in user-controlled
+    # namespaces. Methods(or objects or attributes) like: __init__, __str__,
+    # __repr__ etc. are called special methods (or sometimes called dunder methods)
+    
+    # Basic initializer
+    def __init__(self, name):
+        # Assign the argument to the instance's name attribute
+        self.name = name
+
+        # Initialize property
+        self._age = 0
+
+    # An instance method. All methods take "self" as the first argument
+    def say(self, msg):
+        print("{name}: {message}".format(name=self.name, message=msg))
+
+    # Another instance method
+    def sing(self):
+        return 'yo... yo... microphone check... one two... one two...'
+
+    # A class method is shared among all instances
+    # They are called with the calling class as the first argument
+    @classmethod
+    def get_species(cls):
+        return cls.species
+
+     # A static method is called without a class or instance reference
+    @staticmethod
+    def grunt():
+        return "*grunt*"
+
+    # A property is just like a getter.
+    @property
+    def age(self):
+        return self._age
+
+    # This allows the property to be set
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    # This allows the property to be deleted
+    @age.deleter
+    def age(self):
+        del self._age
+
+
+# When a Python interpreter reads a source file it executes all its code.
+# This __name__ check makes sure this code block is only executed when this
+# module is the main program.
+if __name__ == '__main__':
+# Instantiate a class
+i = Human(name="Ian")
+i.say("hi")                     # "Ian: hi"
+j = Human("Joel")
+j.say("hello")                  # "Joel: hello"
+
+# Call our class method
+i.say(i.get_species())          # "Ian: H. sapiens"
+
+# Change the shared attribute
+Human.species = "H. neanderthalensis"
+i.say(i.get_species())          # => "Ian: H. neanderthalensis"
+
+
+# Call the static method
+print(Human.grunt())            # => "*grunt*"
+
+# Cannot call static method with instance of object 
+# because i.grunt() will automatically put "self" (the object i) as an argument
+print(i.grunt())                # => TypeError: grunt() takes 0 positional arguments but 1 was given
+
+# Get the property
+i.say(i.age)                    # => "Ian: 42"
+
+
+
 
 
 
