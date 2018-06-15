cdb48f09bf99fb7da73ba048b8878b55a7b43878
softwaretm <beisenov_b_b@mail.ru>
2018-06-15 21:25:34 +0300

    More syntax (dictionaries & sets)

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -33,3 +33,54 @@ d, e, f = 4, 5, 6
 # swap two values
 e, d = d, e  
 
+# Dictionaries 
+filled_dict = {"one": 1, "two": 2, "three": 3}
+
+# keys for dictionaries have to be immutable types. Immutable types include ints, floats, strings, tuples.
+valid_dict = {(1,2,3):[1,2,3]}
+
+# Check for existence of keys (NOT values) in a dictionary with "in"
+"one" in filled_dict
+
+# Use "get()" method to avoid the KeyError
+filled_dict.get("four")
+
+# The get method supports a default argument when the value is missing
+filled_dict.get("four", 4)
+
+# "setdefault()" inserts into a dictionary only if the given key isn't present
+filled_dict.setdefault("five", 5)
+
+# Adding to a dictionary
+filled_dict.update({"four":4})
+filled_dict["four"] = 4
+
+del filled_dict["one"]
+
+empty_set = set()
+some_set = {1, 1, 2, 2, 3, 4}
+
+filled_set = some_set
+filled_set.add(5)
+
+# set intersection with &
+other_set = {3, 4, 5, 6}
+filled_set & other_set  # => {3, 4, 5}
+# set union with |
+filled_set | other_set  # => {1, 2, 3, 4, 5, 6}
+# set difference with -
+{1, 2, 3, 4} - {2, 3, 5}  # => {1, 4}
+# set symmetric difference with ^
+{1, 2, 3, 4} ^ {2, 3, 5}  # => {1, 4, 5}
+# set on the left is a superset of set on the right
+{1, 2} >= {1, 2, 3} # => False
+# set on the left is a subset of set on the right
+{1, 2} <= {1, 2, 3} # => True
+
+# Check for existence in a set with in
+2 in filled_set   # => True
+
+
+
+
+
