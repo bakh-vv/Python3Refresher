5da3ef18bdc872d47e94dd0094fec14298de20d3
softwaretm <beisenov_b_b@mail.ru>
2018-06-16 22:50:22 +0300

    Generators

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -505,6 +505,36 @@ if __name__ == '__main__':
     print('Can I fly? ' + str(sup.fly)) # => Can I fly? False
 
 
+### Generators
+
+# Generators are iterators, a kind of iterable you can only iterate over once. 
+# Generators do not store all the values in memory, they generate the values on the fly
+def double_numbers(iterable):
+    for i in iterable:
+        yield i + i
+
+# Generators are memory-efficient because they only load the data needed to
+# process the next value in the iterable. This allows them to perform
+# operations on otherwise prohibitively large value ranges.
+for i in double_numbers(range(1, 900000000)):  # `range` is a generator.
+    print(i)
+    if i >= 30:
+        break
+
+
+# Just as you can create a list comprehension, you can create generator
+# comprehensions as well.
+values = (-x for x in [1,2,3,4,5])
+for x in values:
+    print(x)  # prints -1 -2 -3 -4 -5 to console/terminal
+
+# You can also cast a generator comprehension directly to a list.
+values = (-x for x in [1,2,3,4,5])
+gen_to_list = list(values)
+print(gen_to_list)  # => [-1, -2, -3, -4, -5]
+
+
+
 
 
 
